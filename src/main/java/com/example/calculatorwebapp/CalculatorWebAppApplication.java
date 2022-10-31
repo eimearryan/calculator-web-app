34369e3b0fed37699a4385ba9742c9c5bef2ba87
eimearryan <ryane38@tcd.ie>
2022-10-31 13:40:20 +0000

    Added Dockerfile

--- src/main/java/com/example/calculatorwebapp/CalculatorWebAppApplication.java
+++ src/main/java/com/example/calculatorwebapp/CalculatorWebAppApplication.java
@@ -1,13 +1,23 @@
 package com.example.calculatorwebapp;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController
 public class CalculatorWebAppApplication {
+
+    @GetMapping()
+    public String Title(){
+        return "Calculator Web Application";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorWebAppApplication.class, args);
     }
 
 }
+
