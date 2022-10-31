4c66f3ae368f2a82b213554f8abf886e7c8a59f8
eimearryan <ryane38@tcd.ie>
2022-10-31 12:54:36 +0000

    intial commit

---
+++ src/main/java/com/example/calculatorwebapp/CalculatorWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorWebAppApplication.class, args);
+    }
+
+}
