bd63a6e62046f2a4e0cf6bae789879a7ded0d191
chloecc10 <98687801+chloecc10@users.noreply.github.com>
2022-11-07 12:46:00 +0000

    Update script.test.js
    Added tests for checkNum()

--- Calculator/script.test.js
+++ Calculator/script.test.js
@@ -1,1 +1,39 @@
+const { scriptName } = require("yargs");
+let checkNum = require("./script");
+let addChar = require("./script");
+let deleteChar = require("./script");
+let compute = require("./script");
 
+test('Testing Sum :', () => {
+expect(checkNum("7+8")).toBe(true)
+});
+
+test('Testing Multiply:', () => {
+const text = checkNum("9*42");
+expect(text).toBe(true)
+});
+
+test('Testing Subtraction:', () => {
+const text = checkNum("9-42");
+expect(text).toBe(true)
+});
+
+test('Testing Brackets:', () => {
+const text = checkNum("(8*42)");
+expect(text).toBe(true)
+});
+    
+test('Testing Power:', () => {
+const text = checkNum("2^6");
+expect(text).toBe(true)
+});
+
+test('Testing Exp:', () => {
+const text = checkNum("(2e6)");
+expect(text).toBe(true)
+});
+
+test('Testing Log:', () => {
+const text = checkNum("(2l6)");
+expect(text).toBe(true)
+});
