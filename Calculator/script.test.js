b254d9147ff2404250f8661198efc91f5bc2555e
chloecc10 <98687801+chloecc10@users.noreply.github.com>
2022-11-08 09:13:07 +0000

    Added testing for addChar and deleteChar function

--- Calculator/script.test.js
+++ Calculator/script.test.js
@@ -1,39 +1,48 @@
-const { scriptName } = require("yargs");
 let checkNum = require("./script");
 let addChar = require("./script");
 let deleteChar = require("./script");
 let compute = require("./script");
 
-test('Testing Sum :', () => {
+test('Testing Sum for CheckNum function:', () => {
 expect(checkNum("7+8")).toBe(true)
 });
 
-test('Testing Multiply:', () => {
+test('Testing Multiply for checkNum function:', () => {
 const text = checkNum("9*42");
 expect(text).toBe(true)
 });
 
-test('Testing Subtraction:', () => {
+test('Testing Subtraction for checkNum function:', () => {
 const text = checkNum("9-42");
 expect(text).toBe(true)
 });
 
-test('Testing Brackets:', () => {
+test('Testing Brackets for checkNum function:', () => {
 const text = checkNum("(8*42)");
 expect(text).toBe(true)
 });
     
-test('Testing Power:', () => {
+test('Testing Power input for checkNum function:', () => {
 const text = checkNum("2^6");
 expect(text).toBe(true)
 });
 
-test('Testing Exp:', () => {
+test('Testing Exp input for checkNum function:', () => {
 const text = checkNum("(2e6)");
 expect(text).toBe(true)
 });
 
-test('Testing Log:', () => {
+test('Testing Log input for checkNum function:', () => {
 const text = checkNum("(2l6)");
 expect(text).toBe(true)
 });
+
+test('Testing addChar function:', () => {
+    const text = addChar('0', '0');
+    expect(text).toBe(true)
+    });
+   
+test('Testing deleteChar function:', () => {
+    const text = deleteChar('2^2');
+    expect(text).toBe(true)
+    });
